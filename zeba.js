f795670d17a29db1794be8b40d6e0f24f7b2bc15
sameer <05sameerk@gmail.com>
2026-07-01 23:59:39 +0530

    added function that greets

---
+++ zeba.js
@@ -0,0 +1,7 @@
+
+const greet = (name, number) => {
+    
+    return `Hello, ${name}! Welcome to the world of JavaScript. 2 + 2 = ${number} ${2 + 2 !== number ? "incorrect" : "correct"}, which is a simple math operation.`;
+}
+
+console.log(greet("Zeba", 4));
